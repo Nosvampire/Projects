01d3993ea86854fa10ab8cb3af6f0085c50290ee
Shinigami <Shinigami@Kenshi-Imamura>
2014-12-07 02:06:57 -0300

    Last Update

--- WebApp TT Biblioteca/src/java/FCD/EstadoFCD.java
+++ WebApp TT Biblioteca/src/java/FCD/EstadoFCD.java
@@ -5,14 +5,14 @@ import java.util.List;
 import javax.faces.model.SelectItem;
 
 public class EstadoFCD {
-    public static List<SelectItem> listResultadoBusqueda(){
-        ArrayList<SelectItem> r = new ArrayList<>();
-        EstadoFCD
-    }
-    
-    public static List<Estado> listEstado(){
-        List<Estado> listAccion;
-        
-        
-    }
+//    public static List<SelectItem> listResultadoBusqueda(){
+//        ArrayList<SelectItem> r = new ArrayList<>();
+//        EstadoFCD
+//    }
+//    
+//    public static List<Estado> listEstado(){
+//        List<Estado> listAccion;
+//        
+//        
+//    }
 }
